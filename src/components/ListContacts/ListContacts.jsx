87dfe74d408e5af6a1886f6cc409fba1fe65ceb0
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-05-10 20:15:04 +0200

    Finish

--- src/components/ListContacts/ListContacts.jsx
+++ src/components/ListContacts/ListContacts.jsx
@@ -1,20 +1,29 @@
+import { Component } from "react";
 import { Button, Contact, ContactsList } from "./ListContacts.styled";
 
-const ListContacts = ({ list, deleteName}) => {   
+class ListContacts extends Component {   
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.list !== this.props.list) {
+      localStorage.setItem('contacts',JSON.stringify(this.props.list))
+    }
+}
   
+render(){  
     return (
       <ContactsList>
-        {list.map(contact => (          
+        {this.props.list.map(contact => (          
             <Contact key = {contact.id}>
               {contact.name} : {contact.number}
             <Button
               id={contact.id}
-              onClick={deleteName}
+              onClick={this.props.deleteName}
             >Delete</Button>
             </Contact>
           
         ))}
       </ContactsList>
     );
+  };
 }
 export default ListContacts;
