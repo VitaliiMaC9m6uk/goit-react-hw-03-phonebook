807ee1911e521bf750b385bd963701dee51a2a17
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2023-05-10 18:18:25 +0200

    started

---
+++ src/components/ListContacts/ListContacts.jsx
@@ -0,0 +1,20 @@
+import { Button, Contact, ContactsList } from "./ListContacts.styled";
+
+const ListContacts = ({ list, deleteName}) => {   
+  
+    return (
+      <ContactsList>
+        {list.map(contact => (          
+            <Contact key = {contact.id}>
+              {contact.name} : {contact.number}
+            <Button
+              id={contact.id}
+              onClick={deleteName}
+            >Delete</Button>
+            </Contact>
+          
+        ))}
+      </ContactsList>
+    );
+}
+export default ListContacts;
